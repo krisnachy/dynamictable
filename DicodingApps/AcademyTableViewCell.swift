45d06046f42f58ae97c6b0888b977bfba5938e29
krisnachy <cahyayuana@gmail.com>
2023-06-12 14:31:05 +0700

    Initial Commit

---
+++ DicodingApps/AcademyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AcademyTableViewCell.swift
+//  DicodingApps
+//
+//  Created by Cahyo on 12/06/23.
+//
+
+import UIKit
+
+class AcademyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ DicodingApps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DicodingApps
+//
+//  Created by Cahyo on 09/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var academyTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
