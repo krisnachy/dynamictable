40fc7bf984be737e4a51de21e86cdbd84eb6aec3
krisnachy <cahyayuana@gmail.com>
2023-06-15 12:04:58 +0700

    add siege

---
+++ DicodingApps/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  DicodingApps
+//
+//  Created by Cahyo on 15/06/23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var academyImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- DicodingApps/ViewController.swift
+++ DicodingApps/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         academyTableView.dataSource = self
+        academyTableView.delegate = self
+        
         academyTableView.register(
             UINib(nibName: "AcademyTableViewCell", bundle: nil), // Isi dengan nama file XIB
             forCellReuseIdentifier: "AcademyCell" // Isi dengan Identifier Cell yang telah ditentukan
@@ -41,4 +43,13 @@ extension ViewController : UITableViewDataSource {
     }
     
     
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(
+        _ tableView: UITableView,
+        didSelectRowAt indexPath: IndexPath
+    ) {
+        performSegue(withIdentifier: "moveToDetail", sender: nil)
+    }
 }
